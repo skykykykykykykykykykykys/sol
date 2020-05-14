ea4c5d435a72233ea0993586e03d45b8cda9bd3e
Ismail F Aziz <25928284+skykykykykykykykykykykys@users.noreply.github.com>
2020-05-14 13:17:53 +0700

    initiating express

---
+++ app.js
@@ -0,0 +1,16 @@
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", function(req, res) {
+    console.log(req);
+    res.send("Hello");
+});
+
+
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+});
+
