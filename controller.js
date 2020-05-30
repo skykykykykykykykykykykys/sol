74a1902973688fa01eec118c045b9c7b387a60ea
Ismail F Aziz <25928284+skykykykykykykykykykykys@users.noreply.github.com>
2020-05-30 07:43:21 +0700

    adding multi-word names handler

--- controller.js
+++ controller.js
@@ -1,20 +1,40 @@
 'use strict';
-var connection = require('./conn');
-var out = require('./res');
 
-var https = require('https');
-const fetch = require('node-fetch');
 const fs = require('fs');
 
 
 exports.read = function(req, res) {
+    //console.log("Masuk ke read!");
+    readStarNames();
+    res.end();
 }
 
 function readStarNames() {
-    var text = fs.readFileSync('./star.txt');
-    var textByLine = text.toString.split("\n");
+    fs.readFile('./star.txt', (err, data) => { 
+        if (err) throw err; 
+        //console.log("masuk ke filesync");
 
-    console.log()
+        var random = Math.floor(Math.random() * Math.floor(1439));
+        var name = data.toString().split("\n")[random];
+
+        name = "\"Edor_Vang\""
+        name.replace(("\"",""));
+        console.log(name);
+
+        //processing name with multiple words
+        if (name.includes("_")) {
+            var names = name.split("_");
+            var euy = names[0];
+            
+            for (var i = 0; i < names.length - 1; i++) {
+                //console.log(names[i]);
+                euy = euy + " " + names[i+1];
+                
+            }
+            //console.log(euy);
+        }
+
+    });
 
 
 }
--- routes.js
+++ routes.js
@@ -3,14 +3,6 @@
 module.exports = function(app) {
     var todoList = require('./controller');
 
-    app.route('/grader')
-        .post(todoList.submitExam);
-
-    app.route('/ranking/:id_exam')
-        .get(todoList.rankings);
-    app.route('/ranking/:id_exam/:id_user')
-        .get(todoList.singleRank);
-
-    app.route('/public')
-        .get()
+    app.route('/')
+        .get(todoList.read)
 };
